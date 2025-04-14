eb6b0386d36b98909ca7701294270ca93147fe17
Jacob Issakov <yakov.issakov@gmail.com>
2025-04-14 10:50:58 +0500

    Modified admin.py file

--- expense_tracker/admin.py
+++ expense_tracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category
+from .models import Expense
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+list_display = ('name') # Поля, которые будут отображаться в списке
+ 
+search_fields = ('name') # Поиск по названию задачи
+list_filter = ('name') # Фильтр по дате создания
+
+@admin.register(Expense)
+class CategoryAdmin(admin.ModelAdmin):
+list_display = ('name', 'amount', 'category', 'date') # Поля, которые будут отображаться в списке
+ 
+search_fields = ('name', 'amount', 'category', 'date') # Поиск по названию задачи
+list_filter = ('name', 'amount', 'category', 'date') # Фильтр по дате создания
