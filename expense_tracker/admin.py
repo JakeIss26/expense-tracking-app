8ebe33b613ebf066e478a0a31a0e9213cee2a4db
Jacob Issakov <yakov.issakov@gmail.com>
2025-04-21 09:22:10 +0500

    Set up views, URLs, and templates for home and about pages Created superuser

--- expense_tracker/admin.py
+++ expense_tracker/admin.py
@@ -4,14 +4,13 @@ from .models import Expense
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-list_display = ('name') # Поля, которые будут отображаться в списке
- 
-search_fields = ('name') # Поиск по названию задачи
-list_filter = ('name') # Фильтр по дате создания
+    list_display = ('name',)
+    search_fields = ('name',)
+    list_filter = ('name',)
+
 
 @admin.register(Expense)
-class CategoryAdmin(admin.ModelAdmin):
-list_display = ('name', 'amount', 'category', 'date') # Поля, которые будут отображаться в списке
- 
-search_fields = ('name', 'amount', 'category', 'date') # Поиск по названию задачи
-list_filter = ('name', 'amount', 'category', 'date') # Фильтр по дате создания
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'category', 'date')
+    search_fields = ('name', 'amount', 'category__name', 'date')
+    list_filter = ('name', 'amount', 'category', 'date')
--- expense_tracker/views.py
+++ expense_tracker/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Category, Expense
 
-# Create your views here.
+def home(request):
+    data = Expense.objects.all()
+    return render(request, 'home.html', {'data': data})
+    
+def about(request):
+    return render(request, 'about.html')
+
